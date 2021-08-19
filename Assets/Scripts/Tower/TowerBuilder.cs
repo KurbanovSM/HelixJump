e6a587923f76f784cc75d1e10f42adffbc428ac4
KurbanovSM <newgvardgame@gmail.com>
2021-08-19 11:37:57 +0300

    /

---
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private float _levelCount;
+    [SerializeField] private GameObject _beam;
+    [SerializeField] private StartPlatform _startPlatform;
+    [SerializeField] private FinishPlatform _finishPlatform;
+    [SerializeField] private Platform[] _platforms;
+    [SerializeField] private Transform _spawnPoint;
+
+    private float _beamHeight = 4;
+    private float _beamEnd = 0.5f;
+    private float _beamScale => _levelCount/2 + _beamEnd + _beamHeight/2;
+
+
+    private void Awake()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, _beamScale, 1);
+
+        Vector3 Position = beam.transform.position;
+        Position.y = beam.transform.localScale.y - _beamHeight;
+
+        SpawnPlatform(_startPlatform, ref Position, _spawnPoint);
+
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref Position, _spawnPoint);
+        }
+
+        SpawnPlatform(_finishPlatform, ref Position, _spawnPoint);
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 position, Transform parent)
+    {
+        Instantiate(platform, position, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        position.y -= 1;
+    }
+
+}
---
+++ Assets/Scripts/Tower/TowerRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerRotator : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                float torque = touch.deltaPosition.x * Time.deltaTime * -_rotateSpeed;
+
+                _rb.MoveRotation(transform.rotation * Quaternion.Euler(transform.rotation.x, torque, transform.rotation.z));
+                //_rb.AddTorque(Vector3.up * torque);
+            }
+        }
+    }
+}
